fcba9a1fbe20ec21af14dc0717e7648674807a92
Hemant Kumar <hkbiet@gmail.com>
2020-06-01 21:50:45 +0530

    1 basic file created and tested

--- test.c
+++ test.c
@@ -1,16 +1,38 @@
 #include<stdio.h>
 
+struct record{
 
-struct Node{
 
-	int i;
-	int* l;
+	int a,b;
+
 
 };
 
+
 int main(){
 
+	FILE *fpw, *fpr;
 
+	
+	fpw = fopen("file.data", "wb");
+
+	struct record MyRecord;
+
+	MyRecord.a = 1;
+	MyRecord.b = 2;
+
+	fwrite(&MyRecord, sizeof(struct record), 1, fpw);
+
+	fclose(fpw);
+	
+
+	fpr = fopen("file.data", "rb");
+
+	fread(&MyRecord, sizeof(struct record), 1, fpr);
+
+	printf("%d", MyRecord.b);
+
+	fclose(fpr);
 
 
 
